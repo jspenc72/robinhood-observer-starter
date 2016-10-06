9beeacce08ad83beb4f6be414cf3584e6ca8019d
Jspenc72 <jspenc72@users.noreply.github.com>
2016-10-05 18:42:25 -0600

    Update Example

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var credentials = {
 
 var Robinhood = require('robinhood-observer')     //Robinhood has not authenticated but can still be used for the unauthenticated subset of the API
 var subscription = Robinhood(null).observeQuote(['AAPL'])
-var subscription = Robinhood(null).observeQuote(['AAPL'])
 .map(quote => quote.results)
 .distinct()                                             //Only use distict results...
 .subscribe(x => {
